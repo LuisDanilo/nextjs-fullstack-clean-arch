0d0d9355c8ec9116bd423e8d17ee3a7c2b51aa3b
Luis Danilo <luis.danilo.95@hotmail.com>
2026-08-12 13:12:56 -0500

    refactor(todos): extract TodoCard component from TodoList
    - move per-item render into reusable TodoCard
    - simplify TodoList map to use TodoCard

--- src/framework/features/list-todos/presentation/TodoList.tsx
+++ src/framework/features/list-todos/presentation/TodoList.tsx
@@ -1,7 +1,6 @@
 import type { TodoDto } from './tododto'
 import { getTodos } from './getTodos.action'
-import { CompleteTodoForm } from '@/framework/features/complete-todos/presentation/CompleteTodoForm'
-import { DeleteTodoForm } from '@/framework/features/delete-todos/presentation/DeleteTodoForm'
+import { TodoCard } from './TodoCard'
 
 export async function TodoList() {
 
@@ -13,15 +12,6 @@ export async function TodoList() {
     </div>
   }
   return <ul className='flex flex-col gap-2'>
-    {todos.map((todo: TodoDto) => (
-        <li key={todo.id} className='flex flex-col gap-2 p-4 border rounded-lg shadow-md'>
-          <span className='text-lg font-semibold'>{todo.title}</span>
-          <span className='text-sm text-gray-600'>{todo.description}</span>
-          <div className='flex gap-2'>
-            <CompleteTodoForm id={todo.id} completed={todo.completed} />
-            <DeleteTodoForm id={todo.id} />
-          </div>
-        </li>  
-    ))}
+    {todos.map((todo: TodoDto) => <TodoCard key={todo.id} todo={todo}/>)}
   </ul>
 }
